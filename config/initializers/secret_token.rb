98f27faf60b184476815446550c6d18f4bfe7f88
Nathan Stier <Nathan.Stier@gmail.com>
2011-01-25 19:14:47 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CrudLab::Application.config.secret_token = '669c7e78c80d4c834724608e75b12588c0634c3bd03cac51c3f20bdd043fce4de814f7da3f669cb331cefe0e0b3d389f7ea3fd63d7f240ec58cdff2ca931fca0'
